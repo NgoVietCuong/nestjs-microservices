ca866f0adcb35d03bf178784ae1b6fa85c79b03b
ngo cuong <ngocuong972000@gmail.com>
2024-08-03 11:00:13 +0700

    first commit

---
+++ apps/reservations/src/models/reservation.schema.ts
@@ -0,0 +1,9 @@
+import { AbstractDocument } from "@app/common";
+import { Schema } from "@nestjs/mongoose";
+
+
+@Schema({ versionKey: false})
+export class Reservation extends AbstractDocument {
+  timestamp: Date;
+  startDate: Date;
+}
