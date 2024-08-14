8806017ffa8008c360fba1866a5f7e1631a49b39
ngo cuong <ngocuong972000@gmail.com>
2024-08-14 17:17:52 +0700

    feature: dockerize auth app

--- apps/auth/src/users/users.module.ts
+++ apps/auth/src/users/users.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from '@app/common';
+import { DatabaseModule, LoggerModule } from '@app/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { UsersRepository } from './users.repository';
 import { UserDocument, UserSchema } from './models/user.schema';
 
 @Module({
-  imports: [DatabaseModule, DatabaseModule.forFeature([{ name: UserDocument.name, schema: UserSchema }])],
+  imports: [LoggerModule, DatabaseModule, DatabaseModule.forFeature([{ name: UserDocument.name, schema: UserSchema }])],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository],
 })
