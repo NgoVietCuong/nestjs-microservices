98af535b17655ff92e3c9d401e20427bafda4e59
ngo cuong <ngocuong972000@gmail.com>
2024-09-16 22:58:13 +0700

    feature: update payment dto

---
+++ libs/common/src/dto/create-charge.dto.ts
@@ -0,0 +1,13 @@
+import Stripe from 'stripe';
+import { IsDefined, IsNotEmptyObject, IsNumber, ValidateNested } from 'class-validator';
+import { CardDto } from './card.dto';
+
+export class CreateChargeDto {
+  @IsDefined()
+  @IsNotEmptyObject()
+  @ValidateNested()
+  card: CardDto;
+
+  @IsNumber()
+  amount: number;
+}
